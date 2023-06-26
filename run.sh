61c1e2d428ea8adee0795889fa137b1fdc7ff62f
David Paul Graham <dpgraham4401@gmail.com>
2023-06-26 11:35:14 -0400

    add migrate subcommand
    database migrations
    add created_date, updated_date, published, author to articles table

--- run.sh
+++ run.sh
@@ -11,7 +11,23 @@ print_usage() {
    echo    "options:"
    echo    "-d, --db                     \tBring up the local development database and expose it on port 5432"
    echo -e "-n, --new-migration  <NAME>  \tCreate new empty migration file(s)"
+   echo -e "-m, --migrate  [<OPTIONS>]   \tCreate new empty migration file(s)"
    echo -e "-h, --help                   \tPrint this help message"
+   echo
+}
+
+# Display migrate CLI arguments
+print_migrate_usage() {
+   echo "--migrate subcommand help"
+   echo "Used to construct the database URL and run migrations"
+   echo
+   echo    "options:"
+   echo -e "--database <DB-NAME>"
+   echo -e "--password <PASSWORD>"
+   echo -e "--user <USER>"
+   echo -e "--host <HOST>"
+   echo -e "--port <PORT>"
+   echo -e "--query <QUERY> query to attach to database URL (e.g., sslmode=disable)"
    echo
 }
 
@@ -40,6 +56,75 @@ create_migration_file(){
   exit 0
 }
 
+
+
+migrate_up(){
+  echo "Parsing database options..."
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -h|--help)
+        print_migrate_usage
+        exit 0
+        ;;
+      --database)
+        database="$2"
+        shift
+        shift
+        ;;
+      --user)
+        user="$2"
+        shift
+        shift
+        ;;
+      --password)
+        password="$2"
+        shift
+        shift
+        ;;
+      --host)
+        host="$2"
+        shift
+        shift
+        ;;
+      --port)
+        port="$2"
+        shift
+        shift
+        ;;
+      --query)
+        query="$2"
+        shift
+        shift
+        ;;
+    esac
+  done
+  # Check if all required options are set
+  if [ -z "$user" ]; then
+      echo "missing CLI argument --user"
+      exit 1
+  fi
+  if [ -z "$database" ]; then
+      echo "missing CLI argument --database"
+      exit 1
+  fi
+  if [ -z "$host" ]; then
+      echo "missing CLI argument --host"
+      exit 1
+  fi
+  if [ -z "$password" ]; then
+      echo "missing CLI argument --password"
+      exit 1
+  fi
+  if [ -z "$port" ]; then
+    # default port
+    port="5432"
+  fi
+
+  echo "Running migrations..."
+  migrate  -database "postgresql://$user:$password@$host:$port/$database?$query" -path "db/migrations" up
+  exit 0
+}
+
 # Parse CLI argument
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -54,6 +139,11 @@ while [[ $# -gt 0 ]]; do
         ;;
     -d|--db)
       start_db
+      ;;
+    -m|--migrate)
+      shift
+      migrate_up "$@"
+      exit 0
       ;;
     -h|--help)
       print_usage
