101c2326b536598766101364fb2324b2e7581b93
David Paul Graham <dpgraham4401@gmail.com>
2023-06-26 11:23:03 -0400

    initial docker-compose and start DB CLI option form run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
+# Globals
+base_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Display help message
 print_usage() {
    echo "Command line utility to help develop the project"
    echo
    echo    "Syntax: $(basename "$0") <option>"
    echo    "options:"
-   echo -e "-n, --new-migration  <NAME>  \t Create new empty migration file(s)"
-   echo -e "-h, --help                   \t Print this help message"
+   echo    "-d, --db                     \tBring up the local development database and expose it on port 5432"
+   echo -e "-n, --new-migration  <NAME>  \tCreate new empty migration file(s)"
+   echo -e "-h, --help                   \tPrint this help message"
    echo
+}
+
+
+start_db(){
+    echo "starting database..."
+    # check if docker is installed
+    if command -v docker> /dev/null 2>&1; then
+        docker_exec=$(command -v docker)
+    else
+      echo "Docker not found"
+      exit 1
+    fi
+    eval "$docker_exec compose -f $base_dir/docker-compose.yaml up postgres -d"
+    exit 0
 }
 
 # Create a new migration file
@@ -19,6 +37,7 @@ create_migration_file(){
   fi
   echo "Creating new migration: $1"
   migrate create -ext sql -dir db/migrations -format unix "$1"
+  exit 0
 }
 
 # Parse CLI argument
@@ -28,21 +47,21 @@ while [[ $# -gt 0 ]]; do
         shift # Move to the next argument
         if [[ -n $1 ]]; then
           create_migration_file "$1"
-          exit 0
         else
           echo "Missing argument value after -n or --new-migration flag."
           exit 1
         fi
-#        create_migration_file
-#        exit 0
         ;;
+    -d|--db)
+      start_db
+      ;;
     -h|--help)
-	  print_usage
-    exit 0
-		;;
+      print_usage
+      exit 0
+      ;;
     *)
       echo "Unknown option $1"
-	  print_usage
+	    print_usage
       exit 1
       ;;
   esac
