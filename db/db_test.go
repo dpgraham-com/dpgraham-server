2d0cc8524ccf5d3bb3fb79cffaae3c9ff7931287
David Paul Graham <dpgraham4401@gmail.com>
2023-07-01 12:10:56 -0400

    successfully migrate test database

--- db/db_test.go
+++ db/db_test.go
@@ -5,16 +5,19 @@ import (
 	"fmt"
 	"github.com/go-playground/assert/v2"
 	"github.com/go-testfixtures/testfixtures/v3"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"os"
 	"testing"
 )
 
-// TestMain is a special function that runs before any tests are run
 var (
 	db       *sql.DB
 	fixtures *testfixtures.Loader
 )
 
+// TestMain is a special function that runs before any tests are run
 func TestMain(m *testing.M) {
 	var err error
 
@@ -24,10 +27,28 @@ func TestMain(m *testing.M) {
 		fmt.Println(err)
 	}
 
+	// migrate database schema
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		fmt.Println("error getting driver", err)
+	}
+
+	migration, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"postgres", driver)
+	if err != nil {
+		fmt.Println("error creating migrations", err)
+	}
+
+	err = migration.Up()
+	if err != nil {
+		fmt.Println("error migrating")
+	}
+
 	fixtures, err = testfixtures.New(
 		testfixtures.Database(db),
-		testfixtures.Dialect("postgres"),   // Available: "postgresql", "timescaledb", "mysql", "mariadb", "sqlite" and "sqlserver"
-		testfixtures.Directory("fixtures"), // The directory containing the YAML files
+		testfixtures.Dialect("postgres"),
+		testfixtures.Directory("fixtures"),
 	)
 	if err != nil {
 		fmt.Println(err)
@@ -46,5 +67,5 @@ func TestQueryArticleById(t *testing.T) {
 	prepareTestDatabase()
 	article, _ := QueryArticle(db, 1)
 	fmt.Println(article)
-	assert.Equal(t, 1, 1)
+	assert.Equal(t, article.Id, 1)
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-playground/assert/v2 v2.2.0
 	github.com/go-testfixtures/testfixtures/v3 v3.9.0
+	github.com/golang-migrate/migrate/v4 v4.16.2
 	github.com/lib/pq v1.10.9
 )
 
@@ -25,6 +26,8 @@ require (
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
@@ -43,6 +46,7 @@ require (
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	go.opentelemetry.io/otel v1.15.0 // indirect
 	go.opentelemetry.io/otel/trace v1.15.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.10.0 // indirect
 	golang.org/x/net v0.11.0 // indirect
