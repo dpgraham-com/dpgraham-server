106fb709ce7bb5f5fc09a6e40fe7ff0c77fb2060
David Paul Graham <dpgraham4401@gmail.com>
2023-07-09 12:19:17 -0400

    initial cloud dns

--- infra/main.tf
+++ infra/main.tf
@@ -50,3 +50,63 @@ resource "google_sql_user" "users" {
   name     = var.db_username
   password = var.db_password
 }
+
+# CDN tutorial
+#resource "random_id" "rnd" {
+#  byte_length = 4
+#}
+
+# Example apache server we'll use to test Cloud DNS
+resource "google_compute_instance" "default" {
+  name         = "dns-compute-instance"
+  machine_type = "e2-micro"
+  zone         = "us-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  metadata_startup_script = <<-EOF
+  sudo apt-get update && \
+  sudo apt-get install apache2 -y && \
+  echo "<!doctype html><html><body><h1>Hello World!</h1></body></html>" > /var/www/html/index.html
+  EOF
+}
+
+# to allow http traffic
+resource "google_compute_firewall" "default" {
+  name    = "allow-http-traffic"
+  network = "default"
+  allow {
+    ports    = ["80"]
+    protocol = "tcp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# to create a DNS zone
+resource "google_dns_managed_zone" "dpgraham_zone" {
+  name          = "dpgraham-zone"
+  dns_name      = "dpgraham.com."
+  description   = "DNS zone following the google create-domain-tutorial"
+  force_destroy = "true"
+}
+
+# to register web-server's ip address in DNS
+resource "google_dns_record_set" "tutorial_record_set" {
+  name         = google_dns_managed_zone.dpgraham_zone.dns_name
+  managed_zone = google_dns_managed_zone.dpgraham_zone.name
+  type         = "A"
+  ttl          = 300
+  rrdatas = [
+    google_compute_instance.default.network_interface[0].access_config[0].nat_ip
+  ]
+}
