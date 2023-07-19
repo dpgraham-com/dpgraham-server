816a5f6782cb176bb7f46ed9f6f8c4b6eb4d4f81
David Paul Graham <dpgraham4401@gmail.com>
2023-07-19 11:17:14 -0400

    fix cloud run vpc access connector field

--- infra/main.tf
+++ infra/main.tf
@@ -27,8 +27,9 @@ resource "google_project_service" "vpcaccess-api" {
 }
 
 resource "google_vpc_access_connector" "dpgraham-vpc-connector" {
-  name = "dpgraham-vpc-connector"
-  network = google_compute_network.vpc.name
+  name          = "dpgraham-vpc-connector"
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.14.0.0/28"
 }
 
 resource "google_sql_database_instance" "dpgraham_postgres" {
@@ -137,15 +138,15 @@ resource "google_artifact_registry_repository" "dpgraham_com" {
 
 data "google_iam_policy" "noauth" {
   binding {
-    role = "roles/run.invoker"
+    role    = "roles/run.invoker"
     members = ["allUsers"]
   }
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_v2_service.server.location
-  project     = google_cloud_run_v2_service.server.project
-  service     = google_cloud_run_v2_service.server.name
+  location = google_cloud_run_v2_service.server.location
+  project  = google_cloud_run_v2_service.server.project
+  service  = google_cloud_run_v2_service.server.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
@@ -158,29 +159,29 @@ resource "google_cloud_run_v2_service" "server" {
     containers {
       image = format("%s-docker.pkg.dev/%s/%s/%s:sha-9d32ef0", google_artifact_registry_repository.dpgraham_com.location, var.project, google_artifact_registry_repository.dpgraham_com.repository_id, var.server_image_name)
       env {
-        name = "DB_PORT"
+        name  = "DB_PORT"
         value = "5432"
       }
+      #      env {
+      #        name = "DB_HOST"
+      #        value = google_sql_database_instance.dpgraham_postgres.ip_address
+      #      }
       env {
-        name = "DB_HOST"
-        value = google_sql_database_instance.dpgraham_postgres.ip_address
-      }
-      env {
-        name = "DB_NAME"
+        name  = "DB_NAME"
         value = google_sql_database.dpgraham_sql.name
       }
       env {
-        name = "DB_USER"
+        name  = "DB_USER"
         value = google_sql_user.users.name
       }
       env {
-        name = "DB_PASSWORD"
+        name  = "DB_PASSWORD"
         value = google_sql_user.users.password
       }
     }
     vpc_access {
-      connector = google_vpc_access_connector.dpgraham-vpc-connector.name
-      egress = "ALL_TRAFFIC"
+      connector = google_vpc_access_connector.dpgraham-vpc-connector.id
+      egress    = "ALL_TRAFFIC"
     }
     scaling {
       # Limit scale up to prevent any cost blow outs!
